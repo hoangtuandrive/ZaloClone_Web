f6b7680acaf717bc77af8696eeb0daeda9eefdf4
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2022-10-23 20:49:12 +0700

    Refactor

--- src/pages/home/components/navbar/index.jsx
+++ src/pages/home/components/navbar/index.jsx
@@ -1,18 +1,16 @@
-import React  from "react";
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
-    return (  
-              
-            <nav className="navbar">
-                <a href="#">Trang chủ</a>
-                <Link to="/">Tính năng</Link>
-                <Link to ="./authResign">Ứng dụng</Link>
-                <Link to ="./chat">Team phát triển</Link>
-                <Link to='./resign'>Đăng ký</Link>
-                <Link to='./login'>Đăng nhập</Link>
-            </nav>
-      
-    );
+  return (
+    <nav className="navbar">
+      <a href="#">Trang chủ</a>
+      <Link to="/">Tính năng</Link>
+      <Link to="./authResign">Ứng dụng</Link>
+      <Link to="./chat">Team phát triển</Link>
+      <Link to="./resign">Đăng ký</Link>
+      <Link to="./login">Đăng nhập</Link>
+    </nav>
+  );
 }
 export default NavBar;
