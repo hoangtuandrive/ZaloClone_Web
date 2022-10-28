a021ddb2d8185466b8b10c5c8de39efc95518d63
Tran Hoang Long <tranhoanglong720@gmail.com>
2022-10-28 13:27:50 +0700

    Message

--- src/components/Compose/index.js
+++ src/components/Compose/index.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, {useEffect, useState,useContext} from "react";
 import "./Compose.css";
 import { Button } from "antd";
+import { ChatRoomUser, Message, ChatRoom } from "../../../src/models";
+import { Auth, DataStore } from "aws-amplify";
+import { AppContext } from "../../context/AppProvider";
 
 export default function Compose(props) {
+  // console.log("Right", props);
+  console.log("chatroom", {props});
+  const [message, setMessage] = useState("");
+  const { selectedRoomId } =useContext(AppContext);
+  // console.log("alo", props.data.id);
+ 
+  console.log(props.chatRoom);
+  const saveNewMessage = async () => {
+    // get all the users of this chatroom
+    const user = await Auth.currentAuthenticatedUser();
+    const newMessage = await DataStore.save(
+      new Message({
+        content: message,
+        userID: user.attributes.sub,
+        chatroomID: selectedRoomId,
+      })
+    );
+    updateLastMessage(newMessage);
+
+    setMessage("");
+  };
+
+  const updateLastMessage = async (newMessage) => {
+    DataStore.save(
+      ChatRoom.copyOf(props.chatRoom, (updatedChatRoom) => {
+        updatedChatRoom.newMessages = 1;
+        updatedChatRoom.LastMessage = newMessage;
+      })
+    );
+  };
+
+
+
+  const onPress = () => {
+      saveNewMessage();
+      
+  };
+
+
   return (
     <div className="compose">
       <div className="left-button"> {props.leftItems} </div>
@@ -11,10 +53,12 @@ export default function Compose(props) {
           type="text"
           className="compose-input"
           placeholder="Type a message"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
           spellCheck="false"
         />
         <div className="right-button" onClick={() => console.log("send")}>
-          <Button type="primary">Send</Button>
+          <Button type="primary" className="btnSend" onClick={onPress} >Send</Button>
         </div>
       </div>
     </div>
