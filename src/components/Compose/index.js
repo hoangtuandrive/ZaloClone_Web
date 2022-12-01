00b7356ca70bdcc0d89349efadf56ced25cbc15a
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2022-12-01 19:25:30 +0700

    fix send

--- src/components/Compose/index.js
+++ src/components/Compose/index.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState, useContext } from "react";
 import "./Compose.css";
 import { Button } from "antd";
 import { ChatRoomUser, Message, ChatRoom } from "../../models";
-import { Auth, DataStore,Storage } from "aws-amplify";
+import { Auth, DataStore, Storage } from "aws-amplify";
 import { AppContext } from "../../context/AppProvider";
 import ToolbarButton from "../ToolbarButton";
 import EmojiPicker, { EmojiStyle } from "emoji-picker-react";
-import { FrownTwoTone,FileAddTwoTone } from "@ant-design/icons";
+import { FrownTwoTone, FileAddTwoTone } from "@ant-design/icons";
 import Emoji from "../Emoji/Emoji";
 import { v4 as uuidv4 } from "uuid";
 
@@ -54,96 +54,88 @@ export default function Compose(props) {
   const onPress = () => {
     saveNewMessage();
   };
-  
+
   const progressCallback = (progress) => {
     setProgress(progress.loaded / progress.total);
   };
-const getBlob = async (uri) => {
-   
-     const link=URL.createObjectURL(uri);
+  const getBlob = async (uri) => {
+    const link = URL.createObjectURL(uri);
     const respone = await fetch(link);
     const blob = await respone.blob();
     return blob;
   };
 
-const sendImage = async () => {
- 
-  if (!image) {
-    return;
+  const sendImage = async () => {
+    if (!image) {
+      return;
+    }
+    const blob = await getBlob(image);
+    const { key } = await Storage.put(`${uuidv4()}.png`, blob, {
+      progressCallback,
+    });
+
+    // send message with image
+    const user = await Auth.currentAuthenticatedUser();
+    const newMessage = await DataStore.save(
+      new Message({
+        content: "Sent Image",
+        image: key,
+        userID: user.attributes.sub,
+        chatroomID: selectedRoomId,
+      })
+    );
+    updateLastMessage(newMessage);
+  };
+  const sendFile = async () => {
+    if (!file) {
+      console.log("123");
+      return;
+    }
+    const blob = await getBlob(file);
+    const { key } = await Storage.put(`${uuidv4()}.docx`, blob, {
+      progressCallback,
+    });
+
+    // send message with image
+    const user = await Auth.currentAuthenticatedUser();
+    const newMessage = await DataStore.save(
+      new Message({
+        content: "Sent File",
+        file: key,
+        userID: user.attributes.sub,
+        chatroomID: selectedRoomId,
+      })
+    );
+    updateLastMessage(newMessage);
+  };
+  function handleFile({ target }) {
+    console.log("file");
+    const loadedfile = target.files[0];
+    // setFile(loadedImage);
+    file = loadedfile;
+    sendFile();
   }
-  const blob = await getBlob(image);
-  const { key } = await Storage.put(`${uuidv4()}.png`, blob, {
-    progressCallback,
-  });
-
-  // send message with image
-  const user = await Auth.currentAuthenticatedUser();
-  const newMessage = await DataStore.save(
-    new Message({
-      content: "Sent Image",
-      image: key,
-      userID: user.attributes.sub,
-      chatroomID: selectedRoomId,
-    })
-  );
-  updateLastMessage(newMessage);
-    
-};
-const sendFile = async () => {
-
-  if (!file) {
-    console.log("123");
-    return;
+  function handleImage({ target }) {
+    console.log("anh");
+    const loadedImage = target.files[0];
+    // setImage(loadedImage);
+    image = loadedImage;
+    sendImage();
   }
-  const blob = await getBlob(file);
-  const { key } = await Storage.put(`${uuidv4()}.docx`, blob, {
-    progressCallback,
-  });
-
-  // send message with image
-  const user = await Auth.currentAuthenticatedUser();
-  const newMessage = await DataStore.save(
-    new Message({
-      content: "Sent File",
-      file: key,
-      userID: user.attributes.sub,
-      chatroomID: selectedRoomId,
-    })
-  );
-  updateLastMessage(newMessage);
-    
-};
-function handleFile({target}){
-  console.log("file");
-  const loadedfile = target.files[0];
-  // setFile(loadedImage);
-  file=loadedfile;
-  sendFile();
-}
-function handleImage({target}){
-  console.log("anh");
-  const loadedImage = target.files[0];
-  // setImage(loadedImage);
-  image=loadedImage;
-  sendImage();
-  
-}
   return (
     <div className="compose">
-      
       {/* <div className="left-button"> {props.leftItems.} </div> */}
       <div className="left-button">
-
-        <label onChange={handleImage}  htmlFor="formIda" >
-         <input name="Anh" type="file" id="formIda" hidden={true} />
-         <ToolbarButton key="photo" icon="ion-ios-camera" />
+        <label onChange={handleImage} htmlFor="formIda">
+          <input name="Anh" type="file" id="formIda" hidden={true} />
+          <ToolbarButton key="photo" icon="ion-ios-camera" />
         </label>
 
         <ToolbarButton key="audio" icon="ion-ios-mic" />
-      
-        <label onChange={handleFile}  htmlFor="formId" >
-         <input name="File" type="file" id="formId" hidden={true} />
-         <FileAddTwoTone
+
+        <label onChange={handleFile} htmlFor="formId">
+          <input name="File" type="file" id="formId" hidden={true} />
+          <FileAddTwoTone
             style={{
               fontSize: 24,
               alignItems: "center",
@@ -162,8 +154,6 @@ function handleImage({target}){
             marginLeft: 11,
           }}
         />
-
-   
       </div>
       <div className="container">
         <input
@@ -173,17 +163,15 @@ function handleImage({target}){
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           spellCheck="false"
-        /> 
-     
+        />
 
         <div className="right-button" onClick={onPress}>
-          <Button type="primary" className="btnSend" onClick={onPress} >Send</Button>
+          <Button type="primary" className="btnSend">
+            Send
+          </Button>
           {/* {props.rightItems} */}
         </div>
       </div>
     </div>
   );
-
 }
-
-
